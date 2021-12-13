6b77314fee489892b4b684a63a76665dc6a6464b
Maxime Poulain <maxime.poulain@smart.coop>
2021-12-13 12:04:29 +0100

    Add web project

---
+++ src/Smart.FA.Catalog.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.FA.Catalog.Web.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+}
